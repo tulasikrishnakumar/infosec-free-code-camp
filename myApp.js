811e09c07fb1e114637a00f62efc33d34c63b756
tulasikrishnakumar <113905796+tulasikrishnakumar@users.noreply.github.com>
2024-10-19 15:26:32 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,8 +2,10 @@ const express = require('express');
 const helmet = require('helmet');
 
 const app = express();
+
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action:'deny'}));
+app.use(helmet.xssFilter());
 
 
 

33de493c665fd7687fbfddd11543d58105117153
tulasikrishnakumar <113905796+tulasikrishnakumar@users.noreply.github.com>
2024-11-05 17:04:33 +0530

    before hsts

--- myApp.js
+++ myApp.js
@@ -8,8 +8,8 @@ app.use(helmet.frameguard({action:'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-const ninetyDaysInSeconds = 90*24*60*60;
-app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
+// const ninetyDaysInSeconds = 90*24*60*60;
+// app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 
 
 

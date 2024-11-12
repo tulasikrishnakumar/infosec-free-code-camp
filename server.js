f7b769b409e3466cfcc254715947500f23de633e
tulasikrishnakumar <113905796+tulasikrishnakumar@users.noreply.github.com>
2024-11-12 18:18:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 app.disable("x-powered-by");
 var fs = require("fs");
 var path = require("path");
-const bcrypt = require("bcrypt");
+var bcrypt = require("bcrypt");
 
 app.use(function (req, res, next) {
   res.set({
